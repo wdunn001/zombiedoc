5c309ab7a0e78a5da760c52055ebdac098ee6215
sabadam32 <sabadam32@gmail.com>
2020-02-01 14:07:22 -0600

    images in

--- src/app/services/body-parts.service.ts
+++ src/app/services/body-parts.service.ts
@@ -8,24 +8,64 @@ export class BodyPartsService {
 
   parts: Part[] = [
     { type: partType.leg,
-      image: 'https://via.placeholder.com/200x200.png?text=Part',
-      name: 'left leg'
+      image: 'assets/body/leg/legs_01.png',
+      name: 'leg_01'
+    },
+    { type: partType.leg,
+      image: 'assets/body/leg/legs_02.png',
+      name: 'leg_02'
+    },
+    { type: partType.leg,
+      image: 'assets/body/leg/legs_03.png',
+      name: 'leg_03'
     },
     { type: partType.arm,
       image: 'assets/body/arm/left_arm_1.jpg',
-      name: 'left arm'
+      name: 'left_arm_1'
     },
     { type: partType.arm,
-      image: 'https://via.placeholder.com/200x200.png?text=Part',
-      name: 'right arm'
+      image: 'assets/body/arm/left_arm_2.jpg',
+      name: 'left_arm_2'
+    },
+    { type: partType.arm,
+      image: 'assets/body/arm/left_arm_3.jpg',
+      name: 'left_arm_3'
+    },
+    { type: partType.arm,
+      image: 'assets/body/arm/right_arm_1.jpg',
+      name: 'right_arm_1'
+    },
+    { type: partType.arm,
+      image: 'assets/body/arm/right_arm_2.jpg',
+      name: 'right_arm_2'
+    },
+    { type: partType.arm,
+      image: 'assets/body/arm/right_arm_3.jpg',
+      name: 'right_arm_3'
     },
     { type: partType.head,
-      image: 'assets/body/head/eyeballhead.jpg',
-      name: 'head'
+      image: 'assets/body/head/head_01.jpg',
+      name: 'head_01'
+    },
+    { type: partType.head,
+      image: 'assets/body/head/head_02.jpg',
+      name: 'head_02'
+    },
+    { type: partType.head,
+      image: 'assets/body/head/head_03.jpg',
+      name: 'head_03'
     },
     { type: partType.torso,
-      image: 'https://via.placeholder.com/200x200.png?text=Part',
-      name: 'torso'
+      image: 'assets/body/torso/body_01.jpg',
+      name: 'body_01'
+    },
+    { type: partType.torso,
+      image: 'assets/body/torso/body_02.jpg',
+      name: 'body_02'
+    },
+    { type: partType.torso,
+      image: 'assets/body/torso/body_03.jpg',
+      name: 'body_03'
     },
   ];
   constructor() { }
