cb8635f7d44b46480875261ff35d3ed08d3f403a
sabadam32 <sabadam32@gmail.com>
2020-01-31 22:23:14 -0600

    added start button and click event"

--- src/app/start-screen/start-screen.component.ts
+++ src/app/start-screen/start-screen.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-start-screen',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StartScreenComponent implements OnInit {
 
-  constructor() { }
+  @HostListener('window:keyup.enter', ['$event'])
+  keyEvent(event: KeyboardEvent) {
+    this.start();
+
+  }
+
+  constructor(private route: Router ) {
+
+   }
 
   ngOnInit() {
+  }
+
+  start() {
+    this.route.navigate(['dialog'] );
   }
 
 }
