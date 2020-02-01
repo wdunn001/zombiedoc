c2478eb8107eb699d18648b55ce80851f3e9a100
sabadam32 <sabadam32@gmail.com>
2020-02-01 15:25:38 -0600

    intro music

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy {
+  subscriptions: Subscription[];
+  url: string;
+  constructor(private router: Router) {
+    this.subscriptions = [
+      this.router.events.subscribe(r => {if(r['url'] !=null){this.url = r['url']};
+    console.log(this.url)})
+    ];
+
+  }
   title = 'zombiedoc';
+  ngOnDestroy() {this.subscriptions.map(s => {s.unsubscribe();});}
 }
