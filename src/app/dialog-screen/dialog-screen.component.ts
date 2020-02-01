27828052762c6a68b489b09a5beb9ff69284ef3f
jason dunn <jason.dunn@vehicletracking.com>
2020-01-31 21:37:55 -0600

    adding components

---
+++ src/app/dialog-screen/dialog-screen.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dialog-screen',
+  templateUrl: './dialog-screen.component.html',
+  styleUrls: ['./dialog-screen.component.scss']
+})
+export class DialogScreenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
