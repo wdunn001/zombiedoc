41bb102289d85001edaa2d2181c1c72cec4b844c
jason dunn <jason.dunn@vehicletracking.com>
2020-01-31 21:34:33 -0600

    adding component stubs and draggable dependency

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StartScreenComponent } from './start-screen/start-screen.component';
+import { DialogScreenComponent } from './dialog-screen/dialog-screen.component';
+import { GameScreenComponent } from './game-screen/game-screen.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StartScreenComponent,
+    DialogScreenComponent,
+    GameScreenComponent
   ],
   imports: [
     BrowserModule,
