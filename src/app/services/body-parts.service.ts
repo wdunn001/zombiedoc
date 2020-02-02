e5be70538bbeef3454fd9dcaf5651938ee263bd8
Jason Dunn <jason.dunn@vehicletracking.com>
2020-02-02 10:24:36 -0600

    adding zombies

--- src/app/services/body-parts.service.ts
+++ src/app/services/body-parts.service.ts
@@ -7,66 +7,110 @@ import { Part, partType } from '../models/part.model';
 export class BodyPartsService {
 
   parts: Part[] = [
-    { type: partType.leg,
-      image: 'assets/body/leg/legs_01.png',
-      name: 'leg_01'
-    },
-    { type: partType.leg,
-      image: 'assets/body/leg/legs_02.png',
-      name: 'leg_02'
-    },
-    { type: partType.leg,
-      image: 'assets/body/leg/legs_03.png',
-      name: 'leg_03'
-    },
     { type: partType.arm,
-      image: 'assets/body/arm/left_arm_1.jpg',
-      name: 'left_arm_1'
-    },
-    { type: partType.arm,
-      image: 'assets/body/arm/left_arm_2.jpg',
-      name: 'left_arm_2'
-    },
-    { type: partType.arm,
-      image: 'assets/body/arm/left_arm_3.jpg',
-      name: 'left_arm_3'
-    },
-    { type: partType.arm,
-      image: 'assets/body/arm/right_arm_1.jpg',
-      name: 'right_arm_1'
-    },
-    { type: partType.arm,
-      image: 'assets/body/arm/right_arm_2.jpg',
-      name: 'right_arm_2'
-    },
-    { type: partType.arm,
-      image: 'assets/body/arm/right_arm_3.jpg',
-      name: 'right_arm_3'
+      image: 'assets/body/arm/Snake.png',
+      name: 'Snake'
+    },    { type: partType.arm,
+      image: 'assets/body/arm/Tattoo.png',
+      name: 'Tattoo'
+    },    { type: partType.arm,
+      image: 'assets/body/arm/Waterwings.png',
+      name: 'Water Wings'
+    },    { type: partType.arm,
+      image: 'assets/body/arm/Zombie_Arm_H_03.png',
+      name: 'Zombie_Arm_H_03'
+    },    { type: partType.arm,
+      image: 'assets/body/arm/Zombie_Arm_H_05.png',
+      name: 'Zombie_Arm_H_05'
+    },    { type: partType.arm,
+      image: 'assets/body/arm/Zombie_Plant_Arm.png',
+      name: 'Plant Arm'
+    },    { type: partType.arm,
+      image: 'assets/body/arm/Zombie_Portal_Arm.png',
+      name: 'Portal Arm'
+    },    { type: partType.arm,
+      image: 'assets/body/arm/Zombie_spagit_arm.png',
+      name: 'Sahgetti Arm'
     },
     { type: partType.head,
-      image: 'assets/body/head/head_01.jpg',
-      name: 'head_01'
+      image: 'assets/body/head/egg_head.png',
+      name: 'Egg Head'
+    },    { type: partType.head,
+      image: 'assets/body/head/Gordon.png',
+      name: 'Gordon'
     },
     { type: partType.head,
-      image: 'assets/body/head/head_02.png',
-      name: 'head_02'
+      image: 'assets/body/head/Itsnotaphase.png',
+      name: `Its Not a Phase Mom!`
     },
     { type: partType.head,
-      image: 'assets/body/head/head_03.jpg',
-      name: 'head_03'
+      image: 'assets/body/head/Zobross.png',
+      name: 'Zobross'
+    },
+    { type: partType.head,
+      image: 'assets/body/head/Zombie_Head_H_01.png',
+      name: 'Zombie_Head_H_01'
+    },
+    { type: partType.head,
+      image: 'assets/body/head/Zombie_Head_H_02.png',
+      name: 'Zombie_Head_H_02'
+    },
+    { type: partType.head,
+      image: 'assets/body/head/Zombie_Head_H_03.png',
+      name: 'Zombie_Head_H_03'
+    },
+    { type: partType.head,
+      image: 'assets/body/head/Zombie_Nail_Head.png',
+      name: 'Pin Head'
     },
     { type: partType.torso,
-      image: 'assets/body/torso/body_01.jpg',
-      name: 'body_01'
+      image: 'assets/body/head/Hole.png',
+      name: 'Hole'
     },
     { type: partType.torso,
-      image: 'assets/body/torso/body_02.jpg',
-      name: 'body_02'
+      image: 'assets/body/head/Sweater.png',
+      name: 'Sweater'
     },
     { type: partType.torso,
-      image: 'assets/body/torso/body_03.jpg',
-      name: 'body_03'
+      image: 'assets/body/head/Zombie_Body_H_01.png',
+      name: 'Zombie_Body_H_01'
     },
+    { type: partType.torso,
+      image: 'assets/body/head/Zombie_Body_H_02.png',
+      name: 'Zombie_Body_H_02'
+    },
+    { type: partType.torso,
+      image: 'assets/body/head/Zombie_Body_hand_crowd.png',
+      name: 'Zombie_Body_hand_crowd'
+    },
+    { type: partType.leg,
+      image: 'assets/body/head/Cowboy.png',
+      name: 'Cowboy'
+    },
+    { type: partType.torso,
+      image: 'assets/body/head/Shorts.png',
+      name: 'Shorts'
+    },
+    { type: partType.torso,
+      image: 'assets/body/head/Zombie_Leg_plant.png',
+      name: 'Plant Legs'
+    },
+    { type: partType.torso,
+      image: 'assets/body/head/Zombie_Legs_H_01.png',
+      name: 'Pants'
+    },
+    { type: partType.torso,
+      image: 'assets/body/head/Zombie_Legs_H_02.png',
+      name: 'Leg Missing'
+    },
+    { type: partType.torso,
+      image: 'assets/body/head/Zombie_Legs_H_03.png',
+      name: 'Squid Legs'
+    },
+    { type: partType.torso,
+      image: 'assets/body/head/Zombie_Legs_noodle.png',
+      name: 'Squid Legs'
+    }
   ];
   constructor() { }
 }
