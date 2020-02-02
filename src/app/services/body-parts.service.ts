2d5141e7b7444ce94557fe9f5e9dd4500336ad52
sabadam32 <rich@formswift.com>
2020-02-02 10:49:34 -0600

    merge

--- src/app/services/body-parts.service.ts
+++ src/app/services/body-parts.service.ts
@@ -7,107 +7,6 @@ import { Part, partType } from '../models/part.model';
 export class BodyPartsService {
 
   parts: Part[] = [
-<<<<<<< HEAD
-    { type: partType.leg,
-      image: 'assets/body/leg/legs_01.png',
-      name: 'Farmer_Pants'
-    },
-    { type: partType.leg,
-      image: 'assets/body/leg/legs_02.png',
-      name: 'Somthins_Fishy'
-    },
-    { type: partType.leg,
-      image: 'assets/body/leg/legs_03.png',
-      name: 'Restless_Trunks'
-    },
-    { type: partType.leg,
-      image: 'assets/body/leg/legs_04.png',
-      name: 'PlantLife'
-    },
-    { type: partType.leg,
-      image: 'assets/body/leg/legs_05.png',
-      name: 'Leg_Day'
-    },
-    { type: partType.leg,
-      image: 'assets/body/leg/legs_06.png',
-      name: 'Noodle_Knockout'
-    },
-    { type: partType.leg,
-      image: 'assets/body/leg/legs_07.png',
-      name: 'Grave_Justice'
-    },
-    { type: partType.arm,
-      image: 'assets/body/arm/arm_01.png',
-      name: 'Bad_Boy'
-    },
-    { type: partType.arm,
-      image: 'assets/body/arm/arm_02.png',
-      name: 'Fully_Tatted'
-    },
-    { type: partType.arm,
-      image: 'assets/body/arm/arm_03.png',
-      name: 'Tentacle_Triceps'
-    },
-    { type: partType.arm,
-      image: 'assets/body/arm/arm_04.png',
-      name: 'Water_Wing'
-    },
-    { type: partType.arm,
-      image: 'assets/body/arm/arm_05.png',
-      name: 'Snake_Game'
-    },
-    { type: partType.arm,
-      image: 'assets/body/arm/arm_06.png',
-      name: 'Pirhana_Plant'
-    },
-    { type: partType.arm,
-      image: 'assets/body/arm/arm_07.png',
-      name: 'Portal_Gun'
-    },
-    { type: partType.arm,
-      image: 'assets/body/arm/arm_08.png',
-      name: 'My_Spaget'
-    },
-    { type: partType.head,
-      image: 'assets/body/head/Head_01.png',
-      name: 'Brainiac'
-    },
-    { type: partType.head,
-      image: 'assets/body/head/Head_02.png',
-      name: 'Whata_Sucker'
-    },
-    { type: partType.head,
-      image: 'assets/body/head/Head_03.png',
-      name: 'Gordon_Decay'
-    },
-    { type: partType.head,
-      image: 'assets/body/head/Head_04.png',
-      name: 'Cooky_Codger'
-    },
-    { type: partType.head,
-      image: 'assets/body/head/Head_05.png',
-      name: 'Angsty_Undead'
-    },
-    { type: partType.head,
-      image: 'assets/body/head/Head_06.png',
-      name: 'Zom_Ross'
-    },
-    { type: partType.torso,
-      image: 'assets/body/torso/body_01.png',
-      name: 'Puppy_Love'
-    },
-    { type: partType.torso,
-      image: 'assets/body/torso/body_02.png',
-      name: 'Calamari_Casket'
-    },
-    { type: partType.torso,
-      image: 'assets/body/torso/body_03.png',
-      name: 'See_Through'
-    },
-    { type: partType.torso,
-      image: 'assets/body/torso/body_04.png',
-      name: 'Happy_Sweater'
-=======
     { type: partType.arm,
       image: 'assets/body/arm/Snake.png',
       name: 'Snake'
@@ -175,7 +74,6 @@ export class BodyPartsService {
     { type: partType.torso,
       image: 'assets/body/head/Zombie_Body_H_01.png',
       name: 'Zombie_Body_H_01'
->>>>>>> e5be70538bbeef3454fd9dcaf5651938ee263bd8
     },
     { type: partType.torso,
       image: 'assets/body/head/Zombie_Body_H_02.png',
