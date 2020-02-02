39be5f46ae9577fe9de2cae0d808518e8257a9e6
sabadam32 <rich@formswift.com>
2020-02-02 11:31:57 -0600

    fixed mini game and background

--- src/app/stitch-mini-game/stitch-mini-game.component.ts
+++ src/app/stitch-mini-game/stitch-mini-game.component.ts
@@ -19,7 +19,7 @@ export class StitchMiniGameComponent implements AfterViewInit {
     '#                   #',
     '#    *              #',
     '#                   #',
-    '#        *          #',
+    '#                *  #',
     '#                   #',
     '#                   #',
     '#    *              #',
